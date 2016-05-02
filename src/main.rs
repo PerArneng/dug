557b24a9d0bf2f4b8b7f7b3243e0cf2f4b216746
Per Arneng <per.arneng@scalebit.com>
2016-05-02 23:32:23 +0200

    updated function

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,26 @@ use std::env;
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
-/*
-struct PathSizeSummary {
-    path: &str;
-    size: u64;
+
+struct PathSizeSummary<'a> {
+    path: &'a str,
+    size: u64
 }
 
-fn get_path_sizes() -> u64 {
-    
+fn get_path_sizes<'a, I>(path_strings:I) -> Vec<PathSizeSummary<'a>>
+    where I: Iterator<Item=String>  {
+        
+    for x in path_strings {
+        println!("{}", x)
+    }
+    return vec![];
 }
-*/
+
 
 fn main() {
     println!("dug {}", VERSION);
+    
+    let size = get_path_sizes(env::args().skip(1));
     
     for argument in env::args().skip(1) {
         println!("{}", argument);
